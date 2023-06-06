c5181967afdc19272aea3fefc5e166e0c8beda28
shaowee <leeshaowee@hotmail.com>
2023-06-06 10:50:33 +0800

    Remove unnecessary test

--- src/test/java/com/mvidyn/std/phub/ui/HlbApplicationTests.java
+++ src/test/java/com/mvidyn/std/phub/ui/HlbApplicationTests.java
@@ -1,13 +1,8 @@
 package com.mvidyn.std.phub.ui;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HlbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
